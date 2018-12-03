5761dd37681c0c6f93aa1e18c9edaaaa9399929b
kyungzun <kyungzun@Macui-MacBook-Pro.local>
2018-12-02 21:54:08 -0500

    zun's header

--- RRT/RRT.h
+++ RRT/RRT.h
@@ -19,16 +19,45 @@
 #define NUMVTX 100000
 #define GOALBIAS 20
 
+/*****************************
+ * Parameters of the planner *
+ ******************************/
+
+#define NUMOFDOF 3
+#define GOALSAMPLE 10    //10%
+#define EPSILON 0.2
+#define CHECKLEN 0.05
+
+/****************************
+ * Constants of the planner *
+ *****************************/
+#define REACH 1
+#define ADVANCED 2
+#define TRAPPED 3
+
+
 //Core RRT Planner
 class RRT
 {
 	public:
 		RRT(double*	map,int xSize,int ySize,int zSize,double* start,double* goal);
 		~RRT();
-		void plan();
+		void planner();
 		void connect(double* newPt);
 		void nearestNeighbor(double* sample);
 		void extend(double* sample);
+    
+        /***************************
+         * NEW DECLARED FUNCTIONS  *
+         ****************************/
+    
+        void random_config(double* map_size);
+        int Extend(kdTree* tree ,double* q_rand );
+        bool validcheck(double* p1, double* p2);
+        bool Check_invalid(double* point);
+        double Distance(double*p1, double*p2);
+        vector<double*> makeplan(kdTree* tree, double* goal);
+        double* Newpoint(double* q_near, double* q_rand);
 
 		//Collision Checking
 		bool validNewConf(double* point);
