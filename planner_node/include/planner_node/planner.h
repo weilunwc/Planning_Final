83ad2fae7d0d879b3e2ca752466fefdc78018809
ejichen <ijwo@andrew.cmu.edu>
2018-12-04 20:56:46 -0500

    in planner add subscriber to contoller

--- planner_node/include/planner_node/planner.h
+++ planner_node/include/planner_node/planner.h
@@ -5,6 +5,10 @@
 #include <ros/ros.h>
 #include <ros/time.h>
 #include <planner_node/planner_pub.h>
+#include <octomap/octomap.h>
+#include <octomap/OcTree.h>
+#include <geometry_msgs/Pose.h>
+
 
 namespace planner_node
 {
@@ -13,14 +17,18 @@ class planner_rrt
  public:
   explicit planner_rrt(ros::NodeHandle nh);
   void publish_pos(float x, float y, float z);
+  void subscribe_pos();
+  void current_pos_cb(geometry_msgs::Pose msg_pos);
+  octomap::point3d get_current_pos();
  private:
   //! Turn on publisher.
   void start();
-
+  octomap::point3d current_pos;
   //! Turn off publisher.
   void stop();
   bool enable_;
   ros::Publisher pub_;
+  ros::Subscriber sub_;
   ros::NodeHandle nh_;
 
 };
--- planner_node/src/planner.cpp
+++ planner_node/src/planner.cpp
@@ -3,6 +3,7 @@
 #include <planner_node/planner.h>
 #include <vector>
 #include <algorithm>
+#include <geometry_msgs/Pose.h>
 using namespace octomap;
 using namespace std;
 
@@ -17,6 +18,7 @@ planner_rrt::planner_rrt(ros::NodeHandle nh): enable_(true)
   {
     start();
   }
+  this->nh_ = nh;
 
 }
 
@@ -38,9 +40,26 @@ void planner_rrt::publish_pos(float x, float y, float z){
   msg.x = x;
   msg.y = y;
   msg.z = z;
-
   pub_.publish(msg);
 
+}
+
+
+void planner_rrt::subscribe_pos(){
+  // subscriber for current position
+  // geometry_msgs::Pose msg_pos;
+  sub_ = nh_.subscribe("curr_pos", 10000, &planner_rrt::current_pos_cb, this);
+  
+}
+
+void planner_rrt::current_pos_cb(geometry_msgs::Pose msg_pos){
+  octomap::point3d curr_pos((float)msg_pos.position.x , (float)msg_pos.position.y, (float)msg_pos.position.z); 
+  std::cout << "in the subscriber callbacks: " << curr_pos << std::endl;
+  this->current_pos = curr_pos;
+}
+
+octomap::point3d planner_rrt::get_current_pos(){
+  return this->current_pos;
 }
 
 }
@@ -60,11 +79,11 @@ bool check_result(OcTreeNode* node) {
 }
 bool rayCast_collision(octomap::point3d origin, octomap::point3d des_pos, octomap::OcTree* tree){
 	double dr = 0.9; // drone radius
-  	octomap::point3d ray_end;
-  	octomap::point3d dir = origin - des_pos;
-  	octomap::point3d dir_normed = dir;
-  	dir_normed.normalize();
-  	octomap::point3d extend_vec = octomap::point3d(dir_normed.x()*dr, dir_normed.y()*dr, dir_normed.z()*dr);
+  octomap::point3d ray_end;
+  octomap::point3d dir = origin - des_pos;
+  octomap::point3d dir_normed = dir;
+  dir_normed.normalize();
+  octomap::point3d extend_vec = octomap::point3d(dir_normed.x()*dr, dir_normed.y()*dr, dir_normed.z()*dr);
 	octomap::point3d new_des_pos = des_pos - extend_vec;
 	bool success = tree->castRay(origin, -dir, ray_end);
 	// std::cout << dir << -dir << dir_normed << extend_vec
@@ -72,6 +91,26 @@ bool rayCast_collision(octomap::point3d origin, octomap::point3d des_pos, octoma
 	//  << origin.distance(new_des_pos) << " " << origin.distance(ray_end) << std::endl;
 	return success ? origin.distance(new_des_pos) >= origin.distance(ray_end) : false;
 }
+
+octomap::point3d rayCast_collision(octomap::point3d origin, octomap::point3d des_pos, octomap::OcTree* tree, 
+  octomap::point3d ray_end){
+  double dr = 0.9; // drone radius
+  octomap::point3d dir = origin - des_pos;
+  octomap::point3d dir_normed = dir;
+  dir_normed.normalize();
+  octomap::point3d extend_vec = octomap::point3d(dir_normed.x()*dr, dir_normed.y()*dr, dir_normed.z()*dr);
+  octomap::point3d new_des_pos = des_pos - extend_vec;
+  bool success = tree->castRay(origin, -dir, ray_end);
+  // std::cout << dir << -dir << dir_normed << extend_vec
+  //  << des_pos << new_des_pos << ray_end  << " "
+  //  << origin.distance(new_des_pos) << " " << origin.distance(ray_end) << std::endl;
+  bool is_collide = success ? origin.distance(new_des_pos) >= origin.distance(ray_end) : false;
+  return is_collide ? ray_end : new_des_pos;
+
+}
+
+
+
 // Check with ray_casting
 bool is_box_collided(octomap::OcTree* tree, std::vector<float> x, std::vector<float> y, std::vector<float> z, octomap::point3d center, float x_sam, float y_sam, float z_sam) {
         octomap::point3d d(x_sam - center.x(), y_sam - center.y(), z_sam - center.z()); // direction
@@ -112,48 +151,65 @@ int main(int argc, char **argv)
   std::vector<double> x(100, 0), y(100, 0);
   for (int i = 0; i < 100; i++) {
   	x[i] = -0.1 * (double)i;
-  	y[i] = 0.1 * (double)i;
+  }
+
+  for (int j = 0; j < 100; j++){
+  	y[j] = 0.1 * (double)j;
+    
   }
 
 
-	float z_height = 1.0;
+float z_height = 1.0;
 int counter = 0;
 bool flag = true;
 float old_pos_x = 0.0;
 float old_pos_y = 0.0;
 float old_pos_z = z_height;
-bool is_collide;
+bool is_collide = false;
 
 
-// define vertices of the bounding box
-float h_x = 0.4, h_y = 0.4, h_z = 0.1;
-std::vector<float> x_v = {h_x, -h_x, h_x, -h_x, h_x, -h_x, h_x, -h_x};
-std::vector<float> y_v = {h_y, h_y, -h_y, -h_y, h_y, h_y, -h_y, -h_y};
-std::vector<float> z_v = {h_z, h_z, h_z, h_z, -h_z, -h_z, -h_z, -h_z};
+// // define vertices of the bounding box
+// float h_x = 0.4, h_y = 0.4, h_z = 0.1;
+// std::vector<float> x_v = {h_x, -h_x, h_x, -h_x, h_x, -h_x, h_x, -h_x};
+// std::vector<float> y_v = {h_y, h_y, -h_y, -h_y, h_y, h_y, -h_y, -h_y};
+// std::vector<float> z_v = {h_z, h_z, h_z, h_z, -h_z, -h_z, -h_z, -h_z};
 
-float x_sam = 1., y_sam = 0., z_sam = 1.;
-// Test the raycasting function
-octomap::point3d center(0., 0., 1.);
-is_collide = is_box_collided(tree, x_v, y_v, z_v, center, x_sam, y_sam, z_sam);
+// float x_sam = 1., y_sam = 0., z_sam = 1.;
+// // Test the raycasting function
+// octomap::point3d center(0., 0., 1.);
+// is_collide = is_box_collided(tree, x_v, y_v, z_v, center, x_sam, y_sam, z_sam);
 
+std::vector<float> way_x{0.0, -2.0, -4.0, -5.0, -6.0, -7.0, -8.0};
+std::vector<float> way_y{0.0, -2.0, -4.0, -4.0, -4.0, -5.0, -6.0};
+counter = 0;
 while (ros::ok())
 {
-	// counter = min(99, counter);
-	if(counter > 99) break;
-	counter++;
-	octomap::point3d origin(0.0, 0.0, z_height);
-	octomap::point3d target_pos(x[counter], y[counter], z_height);
-	is_collide = rayCast_collision(origin, target_pos, tree);
+	// if(counter ==  way_x.size() - 1) break;
+  octomap::point3d origin((float)0.0, (float)0.0, z_height);
+  // octomap::point3d origin((float)way_x[counter-1], (float)way_y[counter-1], z_height);
+  octomap::point3d target_pos(x[counter], y[counter], z_height);
+  // octomap::point3d target_pos((float)way_x[counter], (float)way_y[counter], z_height);
+  is_collide = rayCast_collision(origin, target_pos, tree);
 
-	std::cout << (is_collide? "Collision detected" : "Not occupied") << std::endl;
-	node.publish_pos((float)x[counter], (float)y[counter], z_height);
-	std::cout << "counter: " << (float)x[counter] << " " << (float)y[counter] << std::endl;
-	if(is_collide){
-		break;
-	}
-	ros::spinOnce();
+  std::cout << (is_collide? "Collision detected" : "No collision") << std::endl;
+  // std::cout << "counter: " << (float)way_x[counter] << " " << (float)way_y[counter] << std::endl;
+  std::cout << "counter: " << (float)x[counter] << " " << (float)y[counter] << std::endl;
+  // node.publish_pos((float)way_x[counter], (float)way_y[counter], z_height);
+  
+  counter++;
+  if (is_collide || counter > 99) {
 
-	loop_rate.sleep();
+    break;
+  }
+
+  octomap::point3d cur_pos = node.get_current_pos();
+  std::cout << "current node:   " << cur_pos << std::endl;
+  node.publish_pos((float)x[counter], (float)y[counter], z_height);
+  ros::spinOnce();
+
+  loop_rate.sleep();
+  node.subscribe_pos();
+  loop_rate.sleep();
 
 	// query = point3d(x[counter], y[counter], 1.);
 	// result = tree->search(query);
